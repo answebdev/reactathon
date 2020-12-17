063c1effa2560565bd58b64f7405959a5c87de1d
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-16 21:25:02 -0800

    react-responsive-modal

--- src/components/pages/ReactDragDrop.js
+++ src/components/pages/ReactDragDrop.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import parse from 'papaparse';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
+
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 import '../styles/DragDrop.css';
 
 const ReactDragDrop = () => {
@@ -13,6 +16,18 @@ const ReactDragDrop = () => {
     { email: 'fake@gmail.com', name: 'Fake' },
   ]);
   // const [contacts, setContacts] = React.useState([]);
+
+  const [open, setOpen] = React.useState(false);
+
+  const closeIcon = (
+    <svg fill='currentColor' viewBox='0 0 20 20' width={28} height={28}>
+      <path
+        fillRule='evenodd'
+        d='M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z'
+        clipRule='evenodd'
+      ></path>
+    </svg>
+  );
 
   const styles = {
     fontSize: '16px',
@@ -84,6 +99,50 @@ const ReactDragDrop = () => {
               </ul>
             </p>
             <hr />
+            <Button variant='dark' onClick={() => setOpen(true)}>
+              Modal
+            </Button>
+
+            <Modal
+              open={open}
+              onClose={() => setOpen(false)}
+              center
+              closeIcon={closeIcon}
+            >
+              <p>
+                This is the React-Responsive-Modal in action. To use it, install
+                it:
+                <br />
+                <strong>npm i react-responsive-modal.</strong>
+                <br />
+                <br />
+                Then, at the top of the file you want to use it in, import it
+                along with the CSS:
+                <br />
+                <strong>import 'react-responsive-modal/styles.css'</strong>
+                <br />
+                <strong>import {Modal} from 'react-responsive-modal' </strong>
+                <br />
+                <br />
+                That's it. Then just add the code for the modal. View the{' '}
+                <a
+                  href='https://react-responsive-modal.leopradel.com/'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  docs&nbsp;
+                </a>
+                and the NPM{' '}
+                <a
+                  href='https://www.npmjs.com/package/react-responsive-modal'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  site
+                </a>
+                .
+              </p>
+            </Modal>
           </div>
 
           <div>
