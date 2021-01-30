e30cf0096296d17862a6fc54bae30c4fb249b425
ANS <36783010+answebdev@users.noreply.github.com>
2021-01-30 13:32:56 -0800

    add copy to clipboard functionality to password generator

--- src/components/pages/PasswordGenerator.js
+++ src/components/pages/PasswordGenerator.js
@@ -3,13 +3,26 @@ import { Helmet } from 'react-helmet';
 import '../styles/PasswordGenerator.css';
 
 class PasswordGenerator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getInitialState();
+  }
+
+  getInitialState = () => ({
+    copyMessage: 'Password copied to clipboard.',
+  });
+
+  resetState = () => {
+    this.setState(this.getInitialState());
+  };
+
   state = {
     userInput: '',
+    copied: '',
   };
 
   handleChange = (event) => {
     this.setState({ userInput: event.target.value });
-
     let stringLength = document.getElementById('characters').value;
     if (isNaN(stringLength)) {
       document.getElementById('warning').innerHTML = 'Please enter a number.';
@@ -17,9 +30,16 @@ class PasswordGenerator extends Component {
       console.log('Valid number entered');
       document.getElementById('warning').innerHTML = '';
     }
+
+    // Limit password to no more than 50 characters (cannot be less than 0).
+    if (stringLength > 50 || stringLength < 1) {
+      document.getElementById('warning').innerHTML =
+        'Please enter a number between 1 and 50.';
+    }
   };
 
   createPassword = (event) => {
+    this.setState({ copied: '' });
     this.setState({ userInput: event.target.value });
     let stringLength = document.getElementById('characters').value;
     console.log(stringLength);
@@ -32,7 +52,47 @@ class PasswordGenerator extends Component {
     }
     // Render the password
     document.getElementById('password').innerHTML = string;
+    document.getElementById('copy-icon').style.display = 'inline';
+
+    // Do not print if value entered is more than 50, or less than 1.
+    if (stringLength > 50 || stringLength < 1) {
+      document.getElementById('password').innerHTML = '';
+      document.getElementById('copy-icon').style.display = 'none';
+    }
+
+    // Do not display copy icon if nothing is entered but button is clicked.
+    if (stringLength === '') {
+      document.getElementById('copy-icon').style.display = 'none';
+    }
+
+    // Do not display copy icon if something other than a number is entered.
+    if (isNaN(stringLength)) {
+      document.getElementById('copy-icon').style.display = 'none';
+    }
   };
+
+  // Copy password to clipboard.
+  // https://stackoverflow.com/questions/56624676/copy-text-between-p-tags-to-clipboard-in-react
+  // https://codesandbox.io/s/wonderful-bird-9rxv3?file=/src/index.js:924-968
+  copyToClipboard = async (e) => {
+    window.getSelection().removeAllRanges();
+    var range = document.createRange();
+    range.selectNode(this.textContent);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    this.setState({ copied: true });
+    // Can put toast / pop up message here?
+    // alert('Copied');
+
+    setTimeout(() => {
+      this.setState({ copyMessage: '' });
+    }, 5000);
+
+    // Reset state of message to be able to get message again when copying more than once
+    this.resetState({ copyMessage: '' });
+  };
+  initRef = (c) => (this.textContent = c);
 
   render() {
     return (
@@ -46,7 +106,6 @@ class PasswordGenerator extends Component {
             <strong>Password Generator</strong>
           </h3>
           <hr />
-
           <input
             type='text'
             id='characters'
@@ -55,14 +114,28 @@ class PasswordGenerator extends Component {
             value={this.state.userInput}
           />
           <p id='instructions'>
-            Please enter the number of characters you want the password to have.
+            Please select the length of your password, from 1 to 50.
           </p>
           <br />
           <button id='pw-btn' onClick={this.createPassword}>
             Generate Password
           </button>
 
-          <p id='password'>{this.state.string}</p>
+          <div className='pw-div'>
+            <i
+              id='copy-icon'
+              onClick={this.copyToClipboard}
+              className='far fa-copy'
+            ></i>
+
+            <p ref={(c) => (this.textContent = c)} id='password'>
+              {this.state.string}
+            </p>
+          </div>
+          <div className='copy-msg'>
+            {this.state.copied && <p>{this.state.copyMessage}</p>}
+          </div>
+
           <p id='warning'></p>
         </div>
       </div>
