87fdd10cf745c3b84a241f356e009f05f8b1fc07
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-15 21:51:53 -0800

    write tests

--- src/components/pages/UsingState.test.js
+++ src/components/pages/UsingState.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UsingState from './UsingState';
+import UsingStateOther from './UsingStateOther';
 
 afterEach(cleanup);
 
@@ -11,6 +12,35 @@ describe('Using State', () => {
     render(<UsingState />);
     screen.debug();
   });
+});
+
+// Unit tests: One assertion (one 'expect') per test.
+// (For functional testing - those that test behavior - you are often doing a series of actions,
+// e.g., click a button and then click a checkbox, and you need this all to be in the same test,
+// even though you may have assertions in between; so you can be more liberal with having more assertions in the same test
+// in functional testing).
+
+test('change greeting button has correct color', () => {
+  render(<UsingStateOther />);
+  // Find the button, using the global object, 'screen', which has access to the virtual DOM created by 'render'.
+  // Find an element with a role of 'button' and text of 'Change Greeting'.
+  const colorButton = screen.getByRole('button', { name: 'Change Greeting' });
+  // Expect the background color to be primary.
+  expect(colorButton).toHaveStyle({ backgroundColor: 'steelblue' });
+});
+
+test('button changes color to blue when clicked', () => {
+  render(<UsingStateOther />);
+  // Find an element with a role of 'button' and text of 'Change Color to blue'.
+  const colorButton = screen.getByRole('button', {
+    name: 'Change Color to blue',
+  });
+  // Click the button.
+  fireEvent.click(colorButton);
+  // Expect background of button to change to blue.
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  // Expect the button text to change to 'Change Color to red'.
+  expect(colorButton.textContent).toBe('Change Color to red');
 });
 
 // SYNC Code Tests (testing the buttons)
--- src/components/pages/UsingStateOther.js
+++ src/components/pages/UsingStateOther.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UsingStateOther = (props) => {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div>
-      <button style={{ marginBottom: '20px' }} onClick={props.textChange}>
+      <button
+        style={{
+          marginBottom: '20px',
+          backgroundColor: 'steelblue',
+          color: '#ffffff',
+        }}
+        onClick={props.textChange}
+      >
         Change Greeting
       </button>
+
       <br />
+
+      <button
+        style={{
+          marginBottom: '20px',
+          backgroundColor: buttonColor,
+          color: '#ffffff',
+        }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change Color to {newButtonColor}
+      </button>
+
+      <br />
+
       <button onClick={props.modaled}>MODAL</button>
     </div>
   );
